f7d145a8f49e8d90909adc51c8d2e2d026f57333
SarahQW <108081862+SarahQW@users.noreply.github.com>
2023-11-11 22:46:25 +0300

    Add styling

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Get all Variables
 //1. Images
-let item = document.querySelector(".cheese-img");
+let cheeseImage = document.querySelector(".cheese-img");
 let ketchupImage = document.querySelector(".ketchup-img");
 let pepperoniImage = document.querySelector(".pepperoni-img");
 let mushroomImage = document.querySelector(".mushroom-img");
@@ -13,74 +13,27 @@ let getScore = document.querySelector(".get-score");
 let contentResipe = document.querySelector(".content-resipe");
 let timeLeft = document.querySelector(".timer");
 //===========================================================
-// forecha in all ingredintes
-pizzaIngredientsImages.forEach((item) => {
-  let currentDroppable = null;
 
-  item.onmousedown = function (event) {
-    let shiftX = event.clientX - item.getBoundingClientRect().left;
-    let shiftY = event.clientY - item.getBoundingClientRect().top;
-
-    item.style.position = "absolute";
-    item.style.zIndex = 1000;
-    document.body.append(item);
-
-    moveAt(event.pageX, event.pageY);
-
-    function moveAt(pageX, pageY) {
-      item.style.left = pageX - shiftX + "px";
-      item.style.top = pageY - shiftY + "px";
-    }
-
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
-
-      item.hidden = true;
-      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-      item.hidden = false;
-
-      if (!elemBelow) return;
-
-      let droppableBelow = elemBelow.closest(".droppable");
-      if (currentDroppable != droppableBelow) {
-        if (currentDroppable) {
-          // null when we were not over a droppable before this event
-          leaveDroppable(currentDroppable);
-        }
-        currentDroppable = droppableBelow;
-        if (currentDroppable) {
-          // null if we're not coming over a droppable now
-          // (maybe just left the droppable)
-          enterDroppable(currentDroppable);
-        }
-      }
-    }
-
-    document.addEventListener("mousemove", onMouseMove);
-
-    item.onmouseup = function () {
-      document.removeEventListener("mousemove", onMouseMove);
-      item.onmouseup = null;
-    };
-  };
-
-  function enterDroppable(elem) {
-    elem.style.background = "pink";
-  }
-
-  function leaveDroppable(elem) {
-    elem.style.background = "";
-  }
-
-  item.ondragstart = function () {
-    return false;
-  };
-});
 // change ingredinets places
+// Tomatouse sausue
+ketchupImage.addEventListener("click", () => {
+  pizzaDoughImage.src = "./image/Pizza-red-souse.png";
+});
+cheeseImage.addEventListener("click", () => {
+  // const pizzaCheese = new
+  pizzaDoughImage.src = "./image/Pizza-cheese.png";
+});
+pepperoniImage.addEventListener("click", () => {
+  // const pizzaCheese = new
+  pizzaDoughImage.src = "./image/pepperoni-pizza.png";
+});
+mushroomImage.addEventListener("click", () => {
+  // const pizzaCheese = new
+  pizzaDoughImage.src = "./image/Pizza-mushroom.png";
+});
+// check if food correct
 
-// check if pizza correct
-
-// check if pizza Not correct
+// check if food Not correct
 
 // Random challenges resipe food
 
